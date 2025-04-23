b896ac1b48b7502128d867252052896153c2cf99
Yeonnies <eliyeon16@gmail.com>
2025-04-24 00:14:51 +0900

    Add: 티빙 앱 생성

---
+++ TvingClone/TvingClone/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TvingClone
+//
+//  Created by 김나연 on 4/24/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.self.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
