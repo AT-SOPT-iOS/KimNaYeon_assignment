df1bc9da8115d6975c6c8584351a97bbe8769014
Yeonnies <eliyeon16@gmail.com>
2025-04-24 02:29:48 +0900

    Add: 폰트 관련 익스텐션 추가

---
+++ TvingClone/TvingClone/Global/Extentions/UITextField+.swift
@@ -0,0 +1,20 @@
+//
+//  Untitled.swift
+//  TvingClone
+//
+//  Created by 김나연 on 4/24/25.
+//
+
+import UIKit
+
+extension UITextField {
+    func setPlaceholderColor(_ placeholderColor: UIColor) {
+        attributedPlaceholder = NSAttributedString(
+            string: placeholder ?? "",
+            attributes: [
+                .foregroundColor: placeholderColor,
+                .font: font
+            ].compactMapValues { $0 }
+        )
+    }
+}
