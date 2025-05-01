2835b156adc75003b6f08b047ffb4a76faa2f1cd
Yeonnies <eliyeon16@gmail.com>
2025-05-02 02:53:32 +0900

    Add: 익스텐션 추가

---
+++ TvingClone/TvingClone/Global/Extentions/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  TvingClone
+//
+//  Created by 김나연 on 5/2/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier : String {
+        return String(describing: self)
+    }
+}
