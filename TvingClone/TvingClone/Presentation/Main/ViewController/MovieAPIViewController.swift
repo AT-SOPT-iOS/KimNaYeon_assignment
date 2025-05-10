9cb1423c6b5cf066016dbd3d6a54440ea01b16ec
Yeonnies <eliyeon16@gmail.com>
2025-05-11 05:04:12 +0900

    Feat: 뷰컨 구현

--- TvingClone/TvingClone/Presentation/Main/ViewController/MovieAPIViewController.swift
+++ TvingClone/TvingClone/Presentation/Main/ViewController/MovieAPIViewController.swift
@@ -12,12 +12,21 @@ import Then
 class MovieAPIViewController: UIViewController {
     
     // MARK: - Property
-    
+    private let tableView = UITableView(frame: .zero, style: .plain)
+    private var movies: [MovieData] = []
 
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
         view.self.backgroundColor = .blackT
+        tableView.do {
+            $0.dataSource = self
+            $0.delegate = self
+            $0.register(MovieAPITableViewCell.self, forCellReuseIdentifier: MovieAPITableViewCell.identifier)
+            $0.backgroundColor = .clear
+        }
+        setStyle()
+        setLayout()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,6 +36,40 @@ class MovieAPIViewController: UIViewController {
     }
     
     // MARK: - SetStyle
+    private func setStyle() {
+        Task {
+            do {
+                let result = try await MovieService.shared.fetchMovieRankList()
+                self.movies = result
+                self.tableView.reloadData()
+            } catch {
+                print("데이터 바인딩 실패:", error)
+            }
+        }
+    }
     
     // MARK: - SetLayout
+    private func setLayout(){
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+extension MovieAPIViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+}
+
+extension MovieAPIViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MovieAPITableViewCell.identifier, for: indexPath) as? MovieAPITableViewCell else { return UITableViewCell() }
+        cell.dataBind(movies[indexPath.row])
+        return cell
+    }
 }
