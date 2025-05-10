608fd70291895278b8ec6a363737e03952b1c801
Yeonnies <eliyeon16@gmail.com>
2025-05-10 23:40:16 +0900

    Feat: 뒤로가기 생성

--- TvingClone/TvingClone/Presentation/Main/ViewController/MovieAPIViewController.swift
+++ TvingClone/TvingClone/Presentation/Main/ViewController/MovieAPIViewController.swift
@@ -6,24 +6,27 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class MovieAPIViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    
+    // MARK: - Property
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.self.backgroundColor = .blackT
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.navigationBar.tintColor = .gray1
+    }
+    
+    // MARK: - SetStyle
+    
+    // MARK: - SetLayout
 }
