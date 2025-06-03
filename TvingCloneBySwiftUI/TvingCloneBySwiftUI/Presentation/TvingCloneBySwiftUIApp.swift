9f6cac8dc3bd401c91d07b56535680b216ae8b1e
Yeonnies <eliyeon16@gmail.com>
2025-06-03 22:18:31 +0900

    Feat: 메인

---
+++ TvingCloneBySwiftUI/TvingCloneBySwiftUI/Presentation/TvingCloneBySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvingCloneBySwiftUIApp.swift
+//  TvingCloneBySwiftUI
+//
+//  Created by 김나연 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TvingCloneBySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
