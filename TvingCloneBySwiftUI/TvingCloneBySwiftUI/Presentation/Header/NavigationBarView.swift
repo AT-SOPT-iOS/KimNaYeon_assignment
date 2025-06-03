66d9b84f76d6624ca535f7371070a35a3196156a
Yeonnies <eliyeon16@gmail.com>
2025-06-03 18:06:43 +0900

    Feat: 헤더 생성

---
+++ TvingCloneBySwiftUI/TvingCloneBySwiftUI/Presentation/Header/NavigationBarView.swift
@@ -0,0 +1,48 @@
+//
+//  NavigationView.swift
+//  TvingCloneBySwiftUI
+//
+//  Created by 김나연 on 6/3/25.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    @State private var navigationSelection = 0
+    let navigationSegments = ["홈", "드라마", "예능", "영화", "스포츠", "뉴스"]
+    @Namespace private var animationNamespace
+
+    var body: some View {
+        HStack(spacing: 28) {
+            ForEach(0..<navigationSegments.count, id: \.self) { index in
+                Button(action: {
+                    withAnimation(.easeInOut) {
+                        navigationSelection = index
+                    }
+                }) {
+                    VStack(spacing: 7) {
+                        Text(navigationSegments[index])
+                            .foregroundColor(.whiteT)
+                            .font(.system(size: 16, weight: .medium))
+
+                        ZStack {
+                            if navigationSelection == index {
+                                Rectangle()
+                                    .fill(Color.whiteT)
+                                    .frame(width: 15, height: 3)
+                                    .matchedGeometryEffect(id: "indicator", in: animationNamespace)
+                            } else {
+                                Color.clear.frame(width: 15, height: 3)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.horizontal, 18)
+    }
+}
+
+#Preview {
+    NavigationBarView()
+}
